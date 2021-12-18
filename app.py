d79f2d97ba68e1935266ab460f0ab75cbc2dad28
Justin Hagerty <wheelsmanx@gmail.com>
2021-12-18 15:40:05 -0500

    added select functionality

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 
 import json
-import serial
+#import serial
+from collections import OrderedDict
+
 
 '''
 Example Arduino Script for reading: (this is likely going to change a lot since we will need to mimic HID witha  different board)
@@ -18,24 +20,37 @@ def createSerialConnection(location, baudRate):
     print(ser.readline())
 
 with open('./content.json') as f:
-  content = json.load(f)
+  content = OrderedDict(json.load(f))
 
 def displayList():
-    for x in content.keys():
-        subjectCounter = 0
-        for y in range(0, len(content[x])):
-            subjectCounter = subjectCounter + 1
-            for z in content[x][y]:
-                print(f'[{subjectCounter}][{z}]\n')
-                for item in range(0, len(content[x][y][z])):
-                    print(f'\t [{item}]{content[x][y][z][item]}')
-                print("\n")
+    for x in enumerate(content):
+        print(f'[{x[0]}]:{x[1]}')
+        for y in enumerate(content[x[1]]):
+            for z in enumerate(y[1]):
+                print(f'\t[{y[0]}]:{z[1]}')
+                for g in enumerate(content[x[1]][y[0]][z[1]]):
+                    print(f'\t\t[{g[0]}]:{g[1]}')
+
+def selectItem(input_dict, input_number):
+    indexKeys = list(input_dict.keys())
+    print(f'[{indexKeys[input_number]}]')
+    return input_dict[indexKeys[input_number]]
+
+def getNumbers(input_list):
+    return [int(x) for x in input_list.split('.')]
 
 def getInput():
     user_message = input("What would you like to say?")
 
+def getSelectedItems(input_list_of_numbers, input_list_of_items):
+    first = selectItem(input_list_of_items, input_list_of_numbers[0])
+    second = selectItem(first[input_list_of_numbers[1]], 0)
+    return second[input_list_of_numbers[2]]
+
 displayList()
-getInput()
+
+while (True):
+    print(getSelectedItems(getNumbers(input("What would you like to select?")), content))
 
 # display list of content
 # get input <number>,<number> or <string-subject>, <number>
